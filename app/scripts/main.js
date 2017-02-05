6489f4f904ed7c2922adc9230a08c634e1d1ae77
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2017-02-05 18:10:40 +0100

    Load collections from prismic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ const prismic = require('./prismic.io');
 
 var loadContent = function(prismicQuery) {
   return prismicQuery([
-    prismic.Prismic.Predicates.any('document.type', [])
+    prismic.Prismic.Predicates.at('document.type', 'exhibition')
   ]).then(function(response) {
     return response.results;
   });
@@ -11,7 +11,37 @@ var loadContent = function(prismicQuery) {
 prismic.getApi()
   .then(function(api) {
     loadContent(api)
-      .then(function(document){
-        console.log(document);
+      .then(function(document) {
+        $.each(document, function(index, value) {
+          var collection = value;
+          var collectioData = {
+            title: collection.getText('exhibition.collection-title'),
+            description: collection.getText('exhibition.collection-description'),
+            image: collection.getImage('exhibition.collection-image').url,
+            thumbnail: collection.getImageView('exhibition.collection-image', 'collection-thumb').url
+          };
+
+          var source   = $("#collection").html();
+          var template = Handlebars.compile(source);
+          $('.collection').append(template(collectioData));
+
+          console.log(collectioData);
+
+          var gallery = collection.getGroup('exhibition.artwork').toArray();
+          gallery.forEach(function(artwork) {
+            var galleryData = {
+              image: artwork.getImage('artwork-image').url,
+              thumbnail: artwork.getImageView('artwork-image', 'artwork-thumb').url,
+              caption: artwork.getText('artwork-caption'),
+              dimensionsX: artwork.getNumber('artwork-dimensions-x'),
+              dimensionsY: artwork.getNumber('artwork-dimensions-y'),
+              year: artwork.getText('artwork-year'),
+              availability: artwork.getBoolean('artwork-availability')
+            };
+            console.log(galleryData);
+          });
+
+        });
+
       })
   });
