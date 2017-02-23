7da2b0936d4aed2d7e46a79096e332d3adabd2f3
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2017-02-23 11:41:47 +0100

    Improve exhibition view

--- app/scripts/view/ExhibitionView.js
+++ app/scripts/view/ExhibitionView.js
@@ -12,10 +12,10 @@ var ExhibitionView = AbstractView.extend({
         id: collection.id,
         description: collection.getText('exhibition.collection-description'),
         image: collection.getImage('exhibition.collection-image').url,
-        thumbnail: collection.getImageView('exhibition.collection-image', 'collection-thumb').url
+        thumbnail: collection.getImageView('exhibition.collection-image', 'collection-thumb').url,
+        artworkCount: collection.getGroup('exhibition.artwork').value.length
       });
     });
-
     return {data: exhibitionData}
   }
 
