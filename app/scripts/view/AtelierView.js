c33c276360fc1928436069a318d7597e70ce3324
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2017-02-28 23:29:05 +0100

    Add Atelier page content

--- app/scripts/view/AtelierView.js
+++ app/scripts/view/AtelierView.js
@@ -1,5 +1,13 @@
 var AtelierView = AbstractView.extend({
 
-  viewTemplate: 'atelier'
+  viewTemplate: 'atelier',
+
+  setup: function(document) {
+    var data = document[0];
+
+    return {
+      content: data.getStructuredText('atelier.atelier').asHtml()
+    };
+  }
 
 });
