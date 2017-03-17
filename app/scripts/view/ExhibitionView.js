a1b4950c310dbb307220a0523397d15f03edccd4
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2017-03-17 18:33:23 +0100

    Add year info to exhibition

--- app/scripts/view/ExhibitionView.js
+++ app/scripts/view/ExhibitionView.js
@@ -11,6 +11,8 @@ var ExhibitionView = AbstractView.extend({
         title: collection.getText('exhibition.collection-title'),
         id: collection.id,
         description: collection.getText('exhibition.collection-description'),
+        yearStart: collection.getText('exhibition.collection-year-start'),
+        yearEnd: collection.getText('exhibition.collection-year-end'),
         thumbnail: collection.getImageView('exhibition.collection-image', 'collection-thumb') ? collection.getImageView('exhibition.collection-image', 'collection-thumb').url : 'images/placeholder.jpg',
         artworkCount: collection.getGroup('exhibition.artwork').value.length
       });
